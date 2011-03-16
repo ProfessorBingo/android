3f064daa3620aa705ffcdb841e55a1cb30943f40
JD Hill <jdh1088@gmail.com>
2011-03-16 12:11:07 -0400

    Added initial android project

---
+++ ProfBingo_21/src/com/profbingo/android/ProfBingo.java
@@ -0,0 +1,13 @@
+package com.profbingo.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfBingo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
