d479d164697e3eaa76b7b08638cc714fd7d666f3
Jimmy Theis <jimmy@jetheis.com>
2011-03-30 07:49:13 -0400

    Implement basic game board

--- ProfBingo_21/src/com/profbingo/android/model/GameBoard.java
+++ ProfBingo_21/src/com/profbingo/android/model/GameBoard.java
@@ -1,6 +1,23 @@
 package com.profbingo.android.model;
 
+import java.util.ArrayList;
+
 
 public class GameBoard {
-
+    
+    private static final int SIZE = 5;
+    
+    private ArrayList<Mannerism> mMannerisms;
+    
+    public GameBoard() {
+        mMannerisms = new ArrayList<Mannerism>((int) Math.pow(SIZE, 2));
+    }
+    
+    public void set(int index, Mannerism m) {
+        mMannerisms.set(index - 1, m);
+    }
+    
+    public Mannerism get(int index) {
+        return mMannerisms.get(index - 1);
+    }
 }
