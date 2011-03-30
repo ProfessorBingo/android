359b5e4670911ff6f093679bdd3357d1291dc4d0
Jimmy Theis <jimmy@jetheis.com>
2011-03-30 07:34:45 -0400

    Fill in Mannerism

--- ProfBingo_21/src/com/profbingo/android/model/Mannerism.java
+++ ProfBingo_21/src/com/profbingo/android/model/Mannerism.java
@@ -2,5 +2,22 @@ package com.profbingo.android.model;
 
 
 public class Mannerism {
+    
+    private int mId;
+    private String mText;
+    
+    public Mannerism(int id, String text) {
+        mId = id;
+        mText = text;
+    }
+    
+    public int getId() { return mId; }
+    public void setId(int id) { mId = id; }
+    
+    public String getText() { return mText; }
+    public void setText(String text) { mText = text; }
 
+    public int compareTo(Professor another) { return getId() - another.getId(); }
+    
+    public String toString() { return getText(); }
 }
