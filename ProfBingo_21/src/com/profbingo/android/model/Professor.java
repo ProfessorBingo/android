cdeba85eec210e97e642e13a30e4012d474c07e8
Jimmy Theis <jimmy@jetheis.com>
2011-03-30 06:58:17 -0400

    Populate Professor model

--- ProfBingo_21/src/com/profbingo/android/model/Professor.java
+++ ProfBingo_21/src/com/profbingo/android/model/Professor.java
@@ -2,5 +2,18 @@ package com.profbingo.android.model;
 
 
 public class Professor {
-
+    
+    private int mId;
+    private String mName;
+    
+    public Professor(int id, String name) {
+        mId = id;
+        mName = name;
+    }
+    
+    public int getId() { return mId; }
+    public void setId(int id) { mId = id; }
+    
+    public String getName() { return mName; }
+    public void setName(String name) { mName = name; }
 }
