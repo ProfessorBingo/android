1df8bb0787b9159b3a1c63ea1538e128cacce70a
Jimmy Theis <jimmy@jetheis.com>
2011-03-30 05:01:56 -0400

    Add home layout

---
+++ ProfBingo_21/src/com/profbingo/android/Home.java
@@ -0,0 +1,15 @@
+package com.profbingo.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Home extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+}
