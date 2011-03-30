2cd9a8a94c20502bb163e00ccdb6f936e9d2c0c0
Jimmy Theis <jimmy@jetheis.com>
2011-03-30 06:40:05 -0400

    Hook up JD's activity

--- ProfBingo_21/src/com/profbingo/android/SelectProf.java
+++ ProfBingo_21/src/com/profbingo/android/SelectProf.java
@@ -1,7 +1,11 @@
 package com.profbingo.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class SelectProf extends Activity {
@@ -10,6 +14,14 @@ public class SelectProf extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_prof);
+        
+        Button playButton = (Button) findViewById(R.id.select_prof_play);
+        
+        playButton.setOnClickListener(new OnClickListener() {
+            
+            public void onClick(View v) {
+                startActivity(new Intent(SelectProf.this, BingoGame.class));
+            }
+        });
     }
-
 }
