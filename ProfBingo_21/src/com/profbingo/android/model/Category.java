e1768b5f6699748c7849383b22c39d57a2172124
Jimmy Theis <jimmy@jetheis.com>
2011-03-30 07:29:15 -0400

    Fill in Category details, make Mannerism

--- ProfBingo_21/src/com/profbingo/android/model/Category.java
+++ ProfBingo_21/src/com/profbingo/android/model/Category.java
@@ -2,5 +2,22 @@ package com.profbingo.android.model;
 
 
 public class Category {
+    
+    private int mId;
+    private String mName;
+    
+    public Category(int id, String name) {
+        mId = id;
+        mName = name;
+    }
+    
+    public int getId() { return mId; }
+    public void setId(int id) { mId = id; }
+    
+    public String getName() { return mName; }
+    public void setName(String name) { mName = name; }
 
+    public int compareTo(Professor another) { return getName().compareTo(another.getName()); }
+    
+    public String toString() { return getName(); }
 }
