13625d159d3372fa34886156e458f57575b6b90e
Jimmy Theis <jimmy@jetheis.com>
2011-03-30 06:49:36 -0400

    Add registration notification

--- ProfBingo_21/src/com/profbingo/android/Register.java
+++ ProfBingo_21/src/com/profbingo/android/Register.java
@@ -1,12 +1,19 @@
 package com.profbingo.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Register extends Activity {
+    
+    private static final String TAG = "ProfBingo.Register";
+    
+    private static final int DIALOG_INFO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +33,29 @@ public class Register extends Activity {
         registerButton.setOnClickListener(new OnClickListener() {
             
             public void onClick(View v) {
-                Register.this.finish();
+                Register.this.showDialog(DIALOG_INFO);
             }
         });
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        super.onCreateDialog(id);
+
+        Dialog dialog = null;
+
+        if (id == DIALOG_INFO) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("You have been registered. Before you can play, you'll need to click the confirmation link in your email.")
+                   .setTitle("Registration Successful")
+                   .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    
+                    public void onClick(DialogInterface dialog, int which) {
+                        Register.this.finish();
+                    }
+                });
+            dialog = builder.create();
+        }
+        return dialog;
     }
 }
